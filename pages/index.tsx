629913b8bfcd4a4d3ad311f823ade7b18b7c57ff
Alexander Frederik Vreeken <alexanderfrederikvreeken@macbook-pro.home>
2021-05-29 21:32:04 +0200

    Adjusted the title of the project for the browser.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Trackthor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
